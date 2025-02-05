16b1911fe1a89ed0bc88e7d238b8edb41195b912
Shamveel-Khan <shamveelkhilji@gmail.com>
2025-02-05 17:54:05 +0500

    initial files

---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main() {
+    return (
+        <section className="main">
+
+        </section>
+    )
+}
---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <section className="header">
+            <h1 className="header-h1">
+                TENZIES
+            </h1>
+        </section>
+    )
+}
