0b475e0ab74bf107d3da4784c9d8de68c9102601
Shamveel-Khan <shamveelkhilji@gmail.com>
2025-02-08 15:42:03 +0500

    halfway there

---
+++ src/components/Dice.jsx
@@ -0,0 +1,5 @@
+export default function Dice(props) {
+    return (
+        <span className="dice">{props.value}</span>
+    )
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,38 @@
+import Dice from "./Dice"
+import React from "react";
+
 export default function Main() {
+    function generateAllNewDice() {
+        let array=[];
+        for(let i=0;i<10;i++) {
+            let num=Math.ceil(Math.random() * 6);
+            array.push(num)
+        }
+        return array
+    }
+    console.log(generateAllNewDice());
+    const [numbers, setNumbers]=React.useState(generateAllNewDice());
+
     return (
         <section className="main">
             <div className="col">
-                <span className="dice">0</span>
-                <span className="dice">0</span>
-                <span className="dice">0</span>
-                <span className="dice">0</span>
-                <span className="dice">0</span>
+                {numbers.filter((_,idx)=> idx < 5).map((item,idx)=> {
+                    return (
+                        <Dice key={idx} value={item} />
+                    )
+                })}
             </div>
             <div className="col">
-                <span className="dice">0</span>
-                <span className="dice">0</span>
-                <span className="dice">0</span>
-                <span className="dice">0</span>
-                <span className="dice">0</span>
+            {numbers.filter((_,idx)=> idx >= 5).map((item,idx)=> {
+                    return (
+                        <Dice key={idx} value={item} />
+                    )
+                })}
             </div>
-            <button className="roll">
+            <button onClick={()=> {
+                setNumbers(generateAllNewDice)
+            }}
+                className="roll">
                 ROLL
             </button>
         </section>
